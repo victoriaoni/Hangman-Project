21297519c1e01169416c61eab7fd49cb4aa537c2
victoria <victoria.oni@hotmail.com>
2022-11-08 10:59:13 +0000

    Milestone 2. Task 3

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,5 @@
 import random
-guess = input("Enter a single letter")
+guess = input("Enter a single letter: ")
 word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
 print(word)
