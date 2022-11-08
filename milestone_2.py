9b88562ba659102a415c3bb5cd1a7e36b1c00661
victoria <victoria.oni@hotmail.com>
2022-11-08 21:49:51 +0000

    Milestone 2 task 4 - check input is single character

--- milestone_2.py
+++ milestone_2.py
@@ -3,3 +3,10 @@ guess = input("Enter a single letter: ")
 word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
 print(word)
+
+if len(guess) == 1 and guess.isalpha():
+      
+      print ('Good guess!')
+else:
+       print ('Oops! That is not a valid input.')
+     
