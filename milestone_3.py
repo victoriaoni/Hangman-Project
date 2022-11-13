c445034e0efe8a5c9a76971accf937f8d4c7f819
victoria <victoria.oni@hotmail.com>
2022-11-13 21:34:59 +0000

    Create functions to run checks

--- milestone_3.py
+++ milestone_3.py
@@ -2,14 +2,34 @@ import random
 
 word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
-#print(word)
+print(word)
+
 while True:
-    guess = input("Guess a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        if guess in word:
-            print("Good guess! " + guess + " is in the word.")
-            
-        else:
-            print("Sorry, " + guess + " is not in the word. Try again.") 
-    break   
+    guess = input("Guess a letter: ").lower()
+    break
+    print(guess)
     
+#Define a function called ask_for_input.
+#Move the code that you wrote in the Iteratively check if the input is a valid guess task into this function block.
+def ask_for_input():
+            if len(guess) == 1 and guess.isalpha():
+                print("I guessed a single letter")  
+     
+# Define a function called check_guess. pass in the guess as a parameter for the function. 
+            def check_guess(guess):
+                if guess in word:
+                    print("Good guess! " + guess + " is in the word.")
+                
+                else:
+                    print("Sorry, " + guess + " is not in the word. Try again.") 
+
+# Outside the while loop, but within the ask_for_input function, call the check_guess function to check if the guess is in the word.     
+    
+            check_guess(guess)  
+
+ask_for_input() 
+
+
+   
+    
+ 
