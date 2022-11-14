eb273904a41737c0a6cd5134653c8d8344bfbefe
victoria <victoria.oni@hotmail.com>
2022-11-14 15:10:17 +0000

    This will continuously ask the user for a letter and validate it.

--- milestone_3.py
+++ milestone_3.py
@@ -1,35 +1,27 @@
 import random
+# Write code that will continuously ask the user for a letter and validate it.
+
+# Step 1. Create a while loop and set the condition to True. 
+# Setting the condition to True ensures that the code run continuously. 
+# In the body of the loop, write the code required for the following steps.
+
+# Step 2: Ask the user to guess a letter and assign this to a variable called guess.
+
+# Step 3. Check that the guess is a single, alphabetical character.
+
+# Step 4. If the guess passes the checks, break out of the loop.
+
+# Step 5: If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
 
 word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
-print(word)
+#print(word)
 
 while True:
-    guess = input("Guess a letter: ").lower()
-    break
-    print(guess)
-    
-#Define a function called ask_for_input.
-#Move the code that you wrote in the Iteratively check if the input is a valid guess task into this function block.
-def ask_for_input():
-            if len(guess) == 1 and guess.isalpha():
-                print("I guessed a single letter")  
-     
-# Define a function called check_guess. pass in the guess as a parameter for the function. 
-            def check_guess(guess):
-                if guess in word:
-                    print("Good guess! " + guess + " is in the word.")
-                
-                else:
-                    print("Sorry, " + guess + " is not in the word. Try again.") 
-
-# Outside the while loop, but within the ask_for_input function, call the check_guess function to check if the guess is in the word.     
-    
-            check_guess(guess)  
-
-ask_for_input() 
-
-
-   
-    
- 
+    guess = input("Guess a letter: ")
+    if len(guess) == 1 and guess.isalpha():
+        if guess in word:
+            print("Good guess! " + guess + " is in the word.")
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.") 
+    break   
