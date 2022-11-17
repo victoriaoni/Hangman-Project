9d3fbbd6b96390a4a7ba8990825077245bac39be
victoria <victoria.oni@hotmail.com>
2022-11-17 23:59:56 +0000

    Create functions to run checks

--- milestone_3.py
+++ milestone_3.py
@@ -2,31 +2,29 @@ import random
 
 # Write code that will continuously ask the user for a letter and validate it.
 
-# Step 1. Create a while loop and set the condition to True. 
-# Setting the condition to True ensures that the code run continuously. 
-# In the body of the loop, write the code required for the following steps.
-# Step 2: Ask the user to guess a letter and assign this to a variable called guess.
-# Step 3. Check that the guess is a single, alphabetical character.
-# Step 4. If the guess passes the checks, break out of the loop.
-# Step 5: If the guess does not pass the checks, 
-# then print a message saying "Invalid letter. Please, enter a single alphabetical character."
-
 word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
-print(word)
+#print(word)
 
-
-def ask_for_input():
-    while True:
-        guess = input("Guess a letter: ").lower()
-        if len(guess) == 1 and guess.isalpha():
+while True:
+    guess = input("Guess a letter: ").lower()    
     
-            def check_guess(guess):
+    def ask_for_input():
+        if len(guess) == 1 and guess.isalpha():
+            ask_for_input()
+    
+    def check_guess(guess):
                 if guess in word:
                     print(f"Good guess! {guess} is in the word.")                 
                 else:
                     print("Invalid letter. Please, enter a single alphabetical character.")  
-        break        
     check_guess(guess)
+    break
+
+
+
+
+
     
-ask_for_input()
+            
+    
