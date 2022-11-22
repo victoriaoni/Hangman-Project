ba31539915a654eaafdd188969987c74ae63aee9
victoria <victoria.oni@hotmail.com>
2022-11-22 21:39:35 +0000

    Check if guessed character in word

--- milestone_3.py
+++ milestone_3.py
@@ -4,26 +4,23 @@ word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
 word=random.choice(word_list)
 #print(word)
 
-while True:
-    guess = input("Guess a letter: ").lower()    
-    
-    def ask_for_input():
-        if len(guess) == 1 and guess.isalpha():
-            #then what??
-            #how do you go on to the next function??
 
-            def check_guess(guess):
-                if guess in word:
-                    print(f"Good guess! {guess} is in the word.")                 
-                else:
-                    print("Invalid letter. Please, enter a single alphabetical character.")  
-                check_guess(guess)
-    
-    break
+while True:
+    guess = input("Guess a letter: ").lower()
+    break    
+#print(guess)
+            
+def check_guess(guess):
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+def ask_for_input():
+    if len(guess) == 1 and guess.isalpha():
+        #print('valid input')
+
+        check_guess(guess)
+            
 ask_for_input()
 
-
-
-    
-            
-    
