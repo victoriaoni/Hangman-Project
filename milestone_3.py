d53ba84e311eb7158535bdc23454aab0b0c86347
victoria <victoria.oni@hotmail.com>
2022-11-30 19:38:52 +0000

    Milestone 3 task 1

--- milestone_3.py
+++ milestone_3.py
@@ -1,26 +1,9 @@
-import random
 
-word_list=['Mango', 'Apple', 'Banana', 'Pineapple', 'Strawberry']
-word=random.choice(word_list)
-#print(word)
-
+#Write code that will continuously ask the user for a letter and validate it.
 
 while True:
-    guess = input("Guess a letter: ").lower()
-    break    
-#print(guess)
-            
-def check_guess(guess):
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-def ask_for_input():
+    guess = input("Guess a letter: ")
     if len(guess) == 1 and guess.isalpha():
-        #print('valid input')
-
-        check_guess(guess)
-            
-ask_for_input()
-
+        break    
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.") 
